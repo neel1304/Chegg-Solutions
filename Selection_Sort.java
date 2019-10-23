33f7be5c9b792a867ea9b8a04c5029ee21d60f91
neel1304 <nilarghar@gmail.com>
2019-10-23 22:20:55 +0530

    Create Selection_Sort.java

---
+++ Selection_Sort.java
@@ -0,0 +1,37 @@
+/*SELECTION SORT IN JAVA*/
+
+
+import java.util.*;
+class Selection_Sort
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int len=0;
+int min=1;
+System.out.println("Enter the length of the array");
+len=sc.nextInt();//Length of the Array
+int arr[]=new int[len];
+System.out.println("Enter the array");
+for(int i=0;i<len;i++)
+arr[i]=sc.nextInt();//Array Input
+  
+for(int i=0;i<len-1;i++)
+{
+for(int j=i+1;j<len;j++)
+{
+if(arr[j]<arr[min])
+{
+min=j;
+}
+}
+int temp=arr[min];
+arr[min]=arr[i];
+arr[i]=temp;
+}
+  
+System.out.println("Array after undergoing Selection Sort : ");
+for(int i=0;i<len;i++)
+System.out.print(arr[i]+" ");
+}
+}
